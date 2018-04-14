b7e42751539a8ea0aa32f951de583c933b7ea342
alice <amw46@njit.edu>
2018-04-14 15:30:31 -0400

    usersdb work in progress

--- UsersDB.php
+++ UsersDB.php
@@ -1,3 +1,18 @@
 <?php
+$query = 'SELECT * FROM accounts';
+$statement = $db->prepare($query);
+$statement->execute();
+$accts = $statement->fetchAll();
+$statement->closeCursor();
 
+require('users.php');
+class UsersDB {
+
+    public static function getUsers() {
+        foreach ($accts as $acct) :
+
+        endforeach;
+    }
+
+} //end class
 ?>
